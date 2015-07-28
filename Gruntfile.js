2d3a1095458a67036e28687facf7b78df27a7bc5
Tiago Alves <letiagoalves@gmail.com>
2015-07-28 22:30:29 +0100

    config grunt and eslint

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        eslint: {
+            options: {
+                configFile: 'conf/eslint.json',
+            },
+            target: ['src/**/*.js', 'examples/**/*.js', 'test/**/*.js']
+        }
+    });
+
+    grunt.registerTask('test', ['eslint']);
+};
