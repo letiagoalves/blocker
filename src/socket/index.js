008ac305572b61f61a43f994c0a7284f7c26760e
Tiago Alves <letiagoalves@gmail.com>
2015-08-03 00:15:58 +0100

    fix MAP_UPDATED broadcast, socket error handler

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var domain = require('domain');
 var socketIO = require('socket.io');
 
 var EVENTS = {
@@ -8,36 +9,51 @@ var EVENTS = {
     MAP_UPDATED: 'MAP_UPDATED'
 };
 
-function handler (server, worldInstance) {
+function handler(server, worldInstance) {
     var io;
 
-    function broadcastMapUpdatedEvent() {
-        io.emit(EVENTS.MAP_UPDATED, worldInstance.getMap());
+    // TODO: except for who caused this trigger
+    function broadcastMapUpdatedEventExceptForSender(socket) {
+        socket.broadcast.emit(EVENTS.MAP_UPDATED, worldInstance.getMap());
     }
 
     function onGetMap(socket) {
         var map = worldInstance.getMap();
-
         socket.emit(EVENTS.MAP, map);
-        broadcastMapUpdatedEvent();
     }
 
     function onGetPartialMap(socket, bounds) {
-        var partialMap = worldInstance.getPartialMap(bounds.minX, bounds.minY, bounds.maxX, bounds.maxY);
-
-        socket.emit(EVENTS.PARTIAL_MAP, partialMap);
-        broadcastMapUpdatedEvent();
+        var partialMap;
+        try {
+            partialMap = worldInstance.getPartialMap(bounds.minX, bounds.minY, bounds.maxX, bounds.maxY);
+            socket.emit(EVENTS.PARTIAL_MAP, partialMap);
+            broadcastMapUpdatedEventExceptForSender(socket);
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     function onUserDisconnected() {
         console.log('user disconnected');
     }
 
+    function onErrorHandler(socket, error) {
+        console.log(arguments);
+        console.error(error);
+    }
+
     function connectionHandler(socket) {
         console.log('user connected');
+        var mDomain = domain.create();
+        mDomain.add(socket);
+        mDomain.on('error', function () {
+            console.log('domain error', arguments);
+        });
+
         socket.on(EVENTS.MAP, onGetMap.bind(null, socket));
         socket.on(EVENTS.PARTIAL_MAP, onGetPartialMap.bind(null, socket));
         socket.on('disconnect', onUserDisconnected);
+        socket.on('error', onErrorHandler.bind(null, socket))
     }
 
     io = socketIO(server);
