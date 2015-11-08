9d27c3c7b2fe5015ddc3a28a4223a6668a5b70e8
Tiago Alves <letiagoalves@gmail.com>
2015-11-08 23:28:11 +0000

    fix examples

--- examples/example.js
+++ examples/example.js
@@ -7,7 +7,7 @@ var parser = blocker.generator.parser;
 var worldInstance1 = parser.parse(worldConfiguration1);
 var server1 = blocker.createServer(1337, worldInstance1);
 
-var worldConfiguration2 = require('./../examples/worlds/report-example4.json');
+var worldConfiguration2 = require('./../examples/worlds/world2.json');
 var worldInstance2 = parser.parse(worldConfiguration2);
 var server2 = blocker.createServer(999, worldInstance2);
 
@@ -23,7 +23,7 @@ function preProcessor(event, data) {
         return data;
     }
 
-    size = 2;
+    size = 1;
 
     return {
         minX: data.x - size,
